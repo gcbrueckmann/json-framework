SBJson/SBJson

Xcode 4 archiving and 32-bit compatibility.
Make SBJson compatible with Xcode 4's archiving feature by enabling
the SKIP_INSTALL build setting at the project level. This will prevent
products from the SBJson project from being included in archives
created for products from projects depending on them.

Add explicit ivars to the following classes so they are compatible
with the 32-bit runtime on OS X:

- SBJsonParser

- SBJsonStreamParser

- SBJsonStreamParserAccumulator

- SBJsonStreamWriter

- SBJsonStreamWriterAccumulator

- SBJsonTokeniser

- SBJsonUTF8Stream

- SBJsonWriter

Classes/SBJsonStreamParserAccumulator.h
@@ -30,7 +30,9 @@
 #import <Foundation/Foundation.h>
 #import "SBJsonStreamParser.h"
 
-@interface SBJsonStreamParserAccumulator : NSObject <SBJsonStreamParserDelegate>
+@interface SBJsonStreamParserAccumulator : NSObject <SBJsonStreamParserDelegate> {
+	id value;
+}
 
 @property (copy) id value;
 

gcbrueckmann/json-framework

Classes/SBJsonStreamParserAccumulator.h
@@ -30,7 +30,9 @@
 #import <Foundation/Foundation.h>
 #import "SBJsonStreamParserAdapter.h"
 
-@interface SBJsonStreamParserAccumulator : NSObject <SBJsonStreamParserAdapterDelegate>
+@interface SBJsonStreamParserAccumulator : NSObject <SBJsonStreamParserAdapterDelegate> {
+	id value;
+}
 
 @property (copy) id value;
 